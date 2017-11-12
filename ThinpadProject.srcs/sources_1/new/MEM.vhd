11fedec56399873ab8267748400c4010a30614a1
zhanghuimeng <zhanghuimeng1997@gmail.com>
2017-11-12 23:35:07 +0800

    Finished MEM module. #7

--- ThinpadProject.srcs/sources_1/new/MEM.vhd
+++ ThinpadProject.srcs/sources_1/new/MEM.vhd
@@ -31,19 +31,36 @@ use IEEE.STD_LOGIC_1164.ALL;
 --library UNISIM;
 --use UNISIM.VComponents.all;
 
+use WORK.INCLUDE.ALL;
+
+-- MEM Module in CPU
+-- Combinatory Logic
+
 entity MEM is
-    Port ( rst : in STD_LOGIC;
-           reg_wt_data_i : in STD_LOGIC;
-           reg_wt_addr_i : in STD_LOGIC;
-           reg_wt_en_i : in STD_LOGIC;
-           reg_wt_data_o : out STD_LOGIC;
-           reg_wt_addr_o : out STD_LOGIC;
-           reg_wt_en_o : out STD_LOGIC);
+    Port ( rst :                in STD_LOGIC;                                       -- Reset
+           reg_wt_en_i :        in STD_LOGIC;                                       -- input register write enable from EX_to_MEM
+           reg_wt_addr_i :      in STD_LOGIC_VECTOR(REG_ADDR_LEN-1 downto 0);       -- input register write address from EX_to_MEM
+           reg_wt_data_i :      in STD_LOGIC_VECTOR(REG_DATA_LEN-1 downto 0);       -- input register write data from EX_to_MEM
+           reg_wt_en_o :        out STD_LOGIC;                                      -- output register write enable to MEM_to_WB
+           reg_wt_addr_o :      out STD_LOGIC_VECTOR(REG_ADDR_LEN-1 downto 0);      -- output register write address to MEM_to_WB
+           reg_wt_data_o :      out STD_LOGIC_VECTOR(REG_DATA_LEN-1 downto 0));     -- output register write data to MEM_to_WB
 end MEM;
 
 architecture Behavioral of MEM is
 
 begin
 
+    process (all)
+    begin
+        if rst = RST_ENABLE then
+            reg_wt_en_o <= REG_WT_DISABLE;
+            reg_wt_addr_o <= REG_ZERO_ADDR;
+            reg_wt_data_o <= REG_ZERO_DATA;
+        else
+            reg_wt_en_o <= reg_wt_en_i;
+            reg_wt_addr_o <= reg_wt_addr_i;
+            reg_wt_data_o <= reg_wt_data_i;
+        end if;
+    end process;
 
 end Behavioral;
