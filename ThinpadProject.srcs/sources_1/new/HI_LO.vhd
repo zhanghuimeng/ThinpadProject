d7246b624e295dd03445b4e0d4aa3b0f8c54e4f1
zhanghuimeng <zhanghuimeng1997@gmail.com>
2017-11-18 16:08:08 +0800

    Added HILO Module. Finish #18

---
+++ ThinpadProject.srcs/sources_1/new/HI_LO.vhd
@@ -0,0 +1,65 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 2017/11/18 15:51:30
+-- Design Name: 
+-- Module Name: HI_LO - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+use WORK.INCLUDE.ALL;
+
+-- HI_LO Module in CPU
+-- Timing logic
+
+entity HI_LO is
+    Port ( clk :        in STD_LOGIC;
+           rst :        in STD_LOGIC;
+           en :         in STD_LOGIC;
+           hi_i :       in STD_LOGIC_VECTOR (REG_DATA_LEN-1 downto 0);
+           lo_i :       in STD_LOGIC_VECTOR (REG_DATA_LEN-1 downto 0);
+           hi_o :       out STD_LOGIC_VECTOR (REG_DATA_LEN-1 downto 0);
+           lo_o :       out STD_LOGIC_VECTOR (REG_DATA_LEN-1 downto 0));
+end HI_LO;
+
+architecture Behavioral of HI_LO is
+
+begin
+    process (clk'event)
+    begin
+        if rising_edge(clk) then
+            if rst = RST_ENABLE then
+                hi_o <= REG_ZERO_DATA;
+                lo_o <= REG_ZERO_DATA;
+            elsif en = CHIP_ENABLE then
+                hi_o <= hi_i;
+                lo_o <= lo_i;
+            end if;
+        end if;
+    end process;
+
+end Behavioral;
